91ab6f2f78a7d3f3fa5075cec672cd14f2d68116
root <cephaspeter37@gmail.com>
2023-04-24 14:13:29 -0700

    adding script that exports a CSV file with employee info

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,47 +1,34 @@
 #!/usr/bin/python3
-"""Script that uses JSONPlaceholder API to get information about an employee"""
+""" Script that uses JSONPlaceholder API to get information about employee """
 import csv
 import requests
-import argparse
+import sys
 
 
-def get_employee_data(user_id: int, base_url: str = 'https://jsonplaceholder.typicode.com/') -> tuple:
-    """
-    Returns a tuple with the employee's name and a list of their completed tasks.
-    """
-    user_endpoint = f'{base_url}users/{user_id}'
-    todos_endpoint = f'{base_url}todos?userId={user_id}'
-    
-    response = requests.get(user_endpoint)
-    response.raise_for_status()  # Raise an exception if the request fails.
-    user_data = response.json()
-    name = user_data['username']
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    response = requests.get(todos_endpoint)
-    response.raise_for_status()
-    tasks_data = response.json()
-    tasks = [[user_id, name, task['completed'], task['title']] for task in tasks_data]
-    
-    return name, tasks
+    userid = sys.argv[1]
+    user = '{}users/{}'.format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
 
+    todos = '{}todos?userId={}'.format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        l_task.append([userid,
+                       name,
+                       task.get('completed'),
+                       task.get('title')])
 
-def save_employee_data_to_csv(data: tuple):
-    """
-    Saves employee data to a CSV file.
-    """
-    user_id, name, tasks = data
-    filename = f'{user_id}.csv'
-    with open(filename, mode='w', newline='') as employee_file:
-        employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        for task in tasks:
+    filename = '{}.csv'.format(userid)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        for task in l_task:
             employee_writer.writerow(task)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Get employee data from JSONPlaceholder API and save it to a CSV file.')
-    parser.add_argument('user_id', type=int, help='The ID of the user to retrieve data for.')
-    args = parser.parse_args()
-
-    employee_data = get_employee_data(args.user_id)
-    save_employee_data_to_csv((args.user_id, *employee_data))
-
