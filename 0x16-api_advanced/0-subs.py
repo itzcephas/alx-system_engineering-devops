485966039c367dee73471cca5c5004a8c2d02418
root <cephaspeter37@gmail.com>
2023-05-09 13:28:48 +0000

    a function that queries the Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
-
 import requests
+import sys
+
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, the function returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom"}
-    response = requests.get(url, headers=headers)
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    else:
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
