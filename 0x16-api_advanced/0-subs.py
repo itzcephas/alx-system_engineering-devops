4a2b59eb94324f44c61ac7024a24b5bfea74f406
root <cephaspeter37@gmail.com>
2023-05-09 13:25:47 +0000

    a function that queries the Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, the function returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
